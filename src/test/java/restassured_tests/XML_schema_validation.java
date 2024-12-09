3cecd69648218085315eff2b9672b5240d5759a4
Bruno Bosshard <bruno.bosshard@pepgo.com>
2024-12-09 19:11:12 +1100

    First commit

---
+++ src/test/java/restassured_tests/XML_schema_validation.java
@@ -0,0 +1,20 @@
+package restassured_tests;
+
+import io.restassured.matcher.RestAssuredMatchers;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class XML_schema_validation {
+
+    @Test
+    void testXmlSchemaValidation()
+    {
+        given()
+
+        .when()
+            .get("https://mocktarget.apigee.net/xml")
+
+        .then()
+            .assertThat().body(RestAssuredMatchers.matchesXsdInClasspath("apigeeXmlSchema.xsd"));
+    }
+}
